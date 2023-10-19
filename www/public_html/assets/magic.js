9151eb1fbedabb2713441306b66145d57999e026
Simon Fels <simon@fels.me>
2023-10-19 16:20:19 +0200

    modal testing

--- www/public_html/assets/magic.js
+++ www/public_html/assets/magic.js
@@ -8,4 +8,14 @@
 
     colorSwitcher.checked = (localStorage.getItem("colorPreference") === '1')
     colorSwitcher.dispatchEvent(new Event('change'))
+
+
 })()
+
+function toggleModal() {
+    let modal = document.querySelector("#modal")
+    modal.classList.toggle("hidden")
+    modal.classList.toggle("opacity-0")
+    modal.classList.toggle("opacity-100")
+    document.body.classList.toggle("overflow-hidden")
+}
